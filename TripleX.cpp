7566377093ec511a4bbd536390cae12087fc6a7f
nhlboy <k.ivashchenko@gmail.com>
2020-08-05 15:08:29 -0400

    Initial commit

---
+++ TripleX.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Wake up, Copper...";
+    std::cout << std::endl;
+    std::cout << "This is TARS.";
+    std::cout << "You need to enter the correct code to break out of the Interstellar.";
+    std::cout << std::endl;
+
+
+    const int a = 4;
+    const int b = 8;
+    const int c = 6;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << sum;
+    std::cout << std::endl;
+    std::cout << product;
+    std::cout << std::endl;
+    
+    return 0;
+}
+
